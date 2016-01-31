df1a9c23623829a73ecd3f932876b54510c275f0
lingyun zhu <453016726@qq.com>
2016-01-31 09:39:19 +0800

    Initial Commit

---
+++ Icook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Icook
+//
+//  Created by Macx on 16/1/31.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Icook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Icook
+//
+//  Created by Macx on 16/1/31.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
